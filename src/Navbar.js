d44150be944ab522eca412e620adcc43b1dd71c8
xaviguasch <xaviguasch@gmail.com>
2019-05-30 23:15:53 +0200

    updated a context dynamically

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
   static contextType = ThemeContext
 
   render() {
-    const { isDarkMode } = this.context
+    const { isDarkMode, toggleTheme } = this.context
     const { classes } = this.props
 
     return (
@@ -27,7 +27,7 @@ class Navbar extends Component {
             <Typography className={classes.title} variant='h6' color='inherit'>
               App Title
             </Typography>
-            <Switch />
+            <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
             <div className={classes.search}>
               <div className={classes.searchIcon}>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -7,11 +7,17 @@ export class ThemeProvider extends Component {
     super(props)
 
     this.state = { isDarkMode: false }
+
+    this.toggleTheme = this.toggleTheme.bind(this)
+  }
+
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode })
   }
 
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state, tastesLikeChicken: true }}>
+      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
         {this.props.children}
       </ThemeContext.Provider>
     )
