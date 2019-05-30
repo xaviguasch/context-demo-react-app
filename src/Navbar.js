21f1364b78140bfab1b3679952e5ea42f391e8fc
xaviguasch <xaviguasch@gmail.com>
2019-05-30 12:06:57 +0200

    added Navbar and Material UI

---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import SearchIcon from '@material-ui/icons/Search'
+import Switch from '@material-ui/core/Switch'
+import { withStyles } from '@material-ui/core/styles'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className={classes.root}>
+        <AppBar position='static' color='primary'>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color='inherit'>
+              <span>🇫🇷</span>
+            </IconButton>
+            <Typography className={classes.title} variant='h6' color='inherit'>
+              App Title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
+export default Navbar
