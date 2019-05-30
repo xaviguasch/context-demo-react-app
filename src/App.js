1fc35fc376ebf2fe1f970fcd96ab43a9feaf58be
xaviguasch <xaviguasch@gmail.com>
2019-05-30 13:16:54 +0200

    added an styled Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import Form from './Form'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <Form />
       </div>
     )
   }
